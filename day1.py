5302bf542bc973d3a476f4dd15bad97cdca2617d
tsitiii <ziontaa9@gmail.com>
2025-12-02 17:53:51 +0300

    answer

--- day1.py
+++ day1.py
@@ -5,31 +5,25 @@ cnt = 0
 #         "L68", "L30", "R48", "L5",
 #         "R60", "L55",
 #         "L1", "L99", "R14", "L82"
-#         ]
+#         ]dial = 50
+cnt = 0
+
 arr = []
 with open("input.txt") as f:
     arr = [line.strip() for line in f]
-# print(arr)
+
 for move in arr:
     num = int(move[1:])
     if move[0] == "L":
-        new_dial = dial-num
-        left = move[0]
-        if new_dial == 0:
-            dial = new_dial
-            cnt += 1
-        if new_dial < 0:
-            n = abs(new_dial) - 1
-            dial = 99 - n
-        else:
-            dial = new_dial
-    elif move[0] == 'R':
+        new_dial = dial - num
+        while new_dial < 0:
+            new_dial += 100
+    else:
         new_dial = dial + num
-        if new_dial > 99:
-            n = new_dial - 99
-            dial = n - 1
-            if dial == 0:
-                cnt += 1
-        else:
-            dial = new_dial
+        while new_dial > 99:
+            new_dial -= 100
+    dial = new_dial
+    if dial == 0:
+        cnt += 1
+
 print(cnt)
